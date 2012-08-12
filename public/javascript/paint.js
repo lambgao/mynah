7ccafa4990bf5d480d5f958a019ea6e92e4fe450
lamb <lamb52gao@gmail.com>
2012-08-12 18:37:15 +0800

    paint.js移除对jquery的依赖；

--- public/javascript/paint.js
+++ public/javascript/paint.js
@@ -4,87 +4,100 @@ var clickDrag = new Array();
 var paint;
 var canvas;
 var context;
+var clearBtn;
 var canvasWidth = 800;
 var canvasHeight = 400;
 prepareCanvas();
 /**
-* Creates a canvas element.
-*/
-function prepareCanvas()
-{
-  canvas =  document.getElementById('canvas');
+ * Creates a canvas element.
+ */
+function prepareCanvas() {
+  canvas = document.getElementById('canvas');
   canvas.setAttribute('width', canvasWidth);
   canvas.setAttribute('height', canvasHeight);
   context = canvas.getContext("2d");
-  
+
+  clearBtn = document.getElementById('clearBtn');
   // Add mouse events
   // ----------------
-  $('#canvas').mousedown(function(e)
-  {
+  var press = function(e) {
     // Mouse down location
     var mouseX = e.pageX - this.offsetLeft;
     var mouseY = e.pageY - this.offsetTop;
-    
+
     paint = true;
     addClick(mouseX, mouseY, false);
     redraw();
-  });
-  
-  $('#canvas').mousemove(function(e){
-    if(paint){
+  };
+
+  var drag = function(e) {
+    if (paint) {
       addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
       redraw();
     }
-  });
-  
-  $('#canvas').mouseup(function(e){
+    // Prevent the whole page from dragging if on mobile
+    e.preventDefault();
+  };
+
+  var release = function(e) {
     paint = false;
-      redraw();
-  });
-  
-  $('#canvas').mouseleave(function(e){
+    redraw();
+  };
+
+  var cancel = function(e) {
     paint = false;
-  });
-  
-  $('#clearCanvas').mousedown(function(e)
-  {
+  };
+
+  var clear = function(e) {
     clickX = new Array();
     clickY = new Array();
     clickDrag = new Array();
-    clearCanvas(); 
-  });
+    clearCanvas();
+  };
+
+  // Add mouse event listeners to canvas element
+  canvas.addEventListener("mousedown", press, false);
+  canvas.addEventListener("mousemove", drag, false);
+  canvas.addEventListener("mouseup", release);
+  canvas.addEventListener("mouseout", cancel, false);
+
+  // Add touch event listeners to canvas element
+  canvas.addEventListener("touchstart", press, false);
+  canvas.addEventListener("touchmove", drag, false);
+  canvas.addEventListener("touchend", release, false);
+  canvas.addEventListener("touchcancel", cancel, false);
+  
+  // Add mouse event listeners to canvas element
+  clearBtn.addEventListener("click", clear);
 }
 
-function addClick(x, y, dragging)
-{
+function addClick(x, y, dragging) {
   clickX.push(x);
   clickY.push(y);
   clickDrag.push(dragging);
 }
 
-function clearCanvas()
-{
+function clearCanvas() {
   context.fillStyle = '#ffffff'; // Work around for Chrome
-  context.fillRect(0, 0, canvasWidth, canvasHeight); // Fill in the canvas with white
-  canvas.width = canvas.width; // clears the canvas 
+  context.fillRect(0, 0, canvasWidth, canvasHeight); // Fill in the canvas with
+  // white
+  canvas.width = canvas.width; // clears the canvas
 }
 
-function redraw()
-{
+function redraw() {
   clearCanvas();
-  
+
   var radius = 5;
   context.strokeStyle = "#000000";
   context.lineJoin = "round";
   context.lineWidth = radius;
-      
-  for(var i=0; i < clickX.length; i++)
-  {   
+
+  for ( var i = 0; i < clickX.length; i++) {
     context.beginPath();
-    if(clickDrag[i] && i){
-      context.moveTo(clickX[i-1], clickY[i-1]);
-    }else{
-      context.moveTo(clickX[i]-1, clickY[i]);
+    if (clickDrag[i] && i) {
+      context.moveTo(clickX[i - 1], clickY[i - 1]);
+    } else {
+      context.moveTo(clickX[i] - 1, clickY[i]);
     }
     context.lineTo(clickX[i], clickY[i]);
     context.closePath();
@@ -92,4 +105,6 @@ function redraw()
   }
 }
 
-document.onselectstart=function(){return false;}
+document.onselectstart = function() {
+  return false;
+}
