ce64ea37427f606e72104c2532025c8639920b43
lamb <lamb52gao@gmail.com>
2012-08-14 14:10:35 +0800

    kill IE678 kill.jpg图片修改

--- public/javascript/killer.js
+++ public/javascript/killer.js
@@ -1,16 +1,19 @@
 var browsers = [ {
+  title : 'Firefox',
+  url : 'http://www.mozillaonline.com/'
+}, {
   title : 'Chrome',
   url : 'http://www.google.com/chrome/?hl=zh-CN'
 }, {
-  title : 'Firefox ',
-  url : 'http://www.mozillaonline.com/'
+  title : 'Safari',
+  url : 'http://www.apple.com.cn/safari/'
 }, {
   title : 'Opera',
   url : 'http://www.opera.com/'
 }, {
-  title : 'Safari',
-  url : 'http://www.apple.com.cn/safari/'
+  title : 'IE9',
+  url : 'http://http://windows.microsoft.com/ie9/'
 } ];
 if ($.browser.msie && parseInt($.browser.version, 10) < 9) {
-  $.kill('image/kill.jpg', 640, 284, 150, 200, browsers);
+  $.kill('image/kill.jpg', 625, 400, 100, 200, browsers);
 }
